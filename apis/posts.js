c4f1f8ea2262554137588ee273dd27fd2f13df9d
Thanushree L <149800761+Thanushree1997@users.noreply.github.com>
2024-02-06 21:40:31 +0530

    Create posts.js

---
+++ apis/posts.js
@@ -0,0 +1,35 @@
+import axios from "./axios-instance";
+
+export async function fetchPosts(limit, page) {
+  const res = await axios.get(
+    `https://academics.newtonschool.co/api/v1/quora/post?limit=${limit}&page=${page}`
+  );
+  return res.data.data;
+}
+
+export async function likePost(postId) {
+  const token = localStorage.getItem("token");
+  const res = await axios.post(
+    `https://academics.newtonschool.co/api/v1/quora/like/${postId}`,
+    {},
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+  console.log(res.data);
+}
+
+export async function fetchComments(postId) {
+  const token = localStorage.getItem("token");
+  const res = await axios.get(
+    `https://academics.newtonschool.co/api/v1/quora/post/${postId}/comments`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+  return res.data.data;
+}
